6154775169542075acfec465a502443e0fe34eb9
ishashah2303 <ishashah2303@gmail.com>
2025-03-21 12:49:22 +0530

    day_3

---
+++ 2115.py
@@ -0,0 +1,62 @@
+class Solution(object):
+    def findAllRecipes(self, recipes, ingredients, supplies):
+        supply_set = set(supplies)
+        changed = True
+        result=[]
+
+        while changed:
+            changed = False
+            for i in range (len(recipes)):
+                if recipes[i] in supply_set:
+                    continue
+                can_make = True    
+                for ing in ingredients[i]:
+                    if ing not in supply_set:
+                        can_make = False
+                        break
+                if can_make:
+                    result.append(recipes[i])
+                    supply_set.add(recipes[i])
+                    changed=True
+        return result
+
+
+'''
+Time Complexity: O(N*M)
+Space Complexity: O(N+M)
+
+Question:
+You have information about n different recipes. You are given a string array recipes and a 2D string array ingredients. The ith recipe has the name recipes[i], and you can create it if you have all the needed ingredients from ingredients[i]. A recipe can also be an ingredient for other recipes, i.e., ingredients[i] may contain a string that is in recipes.
+
+You are also given a string array supplies containing all the ingredients that you initially have, and you have an infinite supply of all of them.
+
+Return a list of all the recipes that you can create. You may return the answer in any order.
+
+Note that two recipes may contain each other in their ingredients.
+
+ 
+
+Example 1:
+
+Input: recipes = ["bread"], ingredients = [["yeast","flour"]], supplies = ["yeast","flour","corn"]
+Output: ["bread"]
+Explanation:
+We can create "bread" since we have the ingredients "yeast" and "flour".
+Example 2:
+
+Input: recipes = ["bread","sandwich"], ingredients = [["yeast","flour"],["bread","meat"]], supplies = ["yeast","flour","meat"]
+Output: ["bread","sandwich"]
+Explanation:
+We can create "bread" since we have the ingredients "yeast" and "flour".
+We can create "sandwich" since we have the ingredient "meat" and can create the ingredient "bread".
+Example 3:
+
+Input: recipes = ["bread","sandwich","burger"], ingredients = [["yeast","flour"],["bread","meat"],["sandwich","meat","bread"]], supplies = ["yeast","flour","meat"]
+Output: ["bread","sandwich","burger"]
+Explanation:
+We can create "bread" since we have the ingredients "yeast" and "flour".
+We can create "sandwich" since we have the ingredient "meat" and can create the ingredient "bread".
+We can create "burger" since we have the ingredient "meat" and can create the ingredients "bread" and "sandwich".
+ 
+
+'''
